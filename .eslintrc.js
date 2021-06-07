68034d92bc8e374d30f2f56686aaf737ed3e93ed
Arran Boyd <arran@arranboyd.tech>
2021-06-07 12:09:45 +0100

    File cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    "jest/globals": true,
+  },
+  plugins: ["jest"],
+  extends: [
+    "airbnb-base",
+    "plugin:jest/all",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
 };
--- __tests__/isImage.spec.js
+++ __tests__/isImage.spec.js
@@ -1,10 +1,21 @@
-const { isImage } = require('../lib/index');
+const { isImage } = require("../lib/index");
 
 describe("isImage function", () => {
-  test("Should only convert image files to webp", () => {
+  it("image file", () => {
+    expect.assertions(1);
+
     const input = { ext: ".png" };
     const output = true;
 
-    expect(isImage(input)).toEqual(output);
+    expect(isImage(input)).toStrictEqual(output);
+  });
+
+  it("non-image file", () => {
+    expect.assertions(1);
+
+    const input = { ext: ".csv" };
+    const output = false;
+
+    expect(isImage(input)).toStrictEqual(output);
   });
 });
